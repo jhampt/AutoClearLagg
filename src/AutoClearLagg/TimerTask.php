b4519fdee5b359fc61d0ed9a13d45b428f15b04b
jhampt <49539815+jhampt@users.noreply.github.com>
2019-05-24 13:31:39 -0400

    use sendmessage instead broadcast message and remove lines

--- src/AutoClearLagg/TimerTask.php
+++ src/AutoClearLagg/TimerTask.php
@@ -1,26 +1,20 @@
 <?php
 declare(strict_types=1);
-
 namespace AutoClearLagg;
-
 use pocketmine\scheduler\Task;
-
 class TimerTask extends Task{
-
     private $plugin;
     private $seconds;
-
     public function __construct(Main $plugin, int $seconds){
         $this->plugin = $plugin;
         $this->seconds = $seconds;
     }
-
     public function onRun(int $currentTick): void{
         $times = is_array($this->plugin->settings->get("times")) ? $this->plugin->settings->get("times") : [];
         if(in_array($this->seconds, $times)){
                 $message = $this->plugin->settings->get("time-left-message");
                 $message = str_replace("{SECONDS}", $this->seconds, $message);
-                $this->plugin->getServer()->broadcastMessage($message);
+                $this->plugin->getServer()->sendMessage($message);
         }
         if($this->seconds == 0){
             if($this->plugin->settings->get("items")) $this->plugin->clearItems();
